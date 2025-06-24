84f5eb02142785c49eba8e501241e39b101c032b
fosterj9vcu <fosterj9@vcu.edu>
2025-06-24 05:40:49 -0400

    Delete core directory

--- core/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User, Interest, Product
-
-admin.site.register(User)
-admin.site.register(Interest)
-admin.site.register(Product)
-
-from .models import Video
-admin.site.register(Video)
