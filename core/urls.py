f334d3e1b2fde54596345ed0c5fb8e59f321da20
fosterj9vcu <fosterj9@vcu.edu>
2025-06-24 05:43:00 -0400

    Add files via upload

---
+++ core/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('select_video/<int:user_id>/', views.select_video, name='select_video'),
+    path('select_user/', views.select_user, name='select_user'),
+    path('watch_video_redirect/', views.watch_video_redirect, name='watch_video_redirect'),
+    path('recommend/<int:user_id>/', views.recommend_products, name='recommend'),
+    path('watch/<int:user_id>/<int:video_id>/', views.watch_video, name='watch_video'),
+]
+
+
